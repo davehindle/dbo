e4d4cd86122dbda76de2a26ce96d380d46642d64
Dave Hindle <dave@isware.co.uk>
2015-12-16 16:13:52 +0000

    Version 1

--- docs/html/search/all_c.js
+++ docs/html/search/all_c.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['ne',['NE',['../classconstraint.html#a7e396087ac8d1599de8c52809d56f601',1,'constraint']]]
+  ['ne',['NE',['../classconstraint.html#a7e396087ac8d1599de8c52809d56f601',1,'constraint']]],
+  ['null',['NULL',['../classconstraint.html#a1d309686038b8d79ba68ce1afeb76c97',1,'constraint']]]
 ];
