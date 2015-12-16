5dfb5fb3f30ca04c67e9f6ff7048ef83950c16aa
Dave Hindle <dave@isware.co.uk>
2015-12-16 10:17:04 +0000

    Rehash of SQL construction

--- lib/dbo.php
+++ lib/dbo.php
@@ -72,7 +72,7 @@ class entity {
 		$this->describe();
 
 		foreach($f->constraints as $c) {
-			if (!array_key_exists($c->column, $this->columns)) throw new exception('Column '.$c->column.' does not exist in table '.$this->schema.'.'.$this->name);
+			if (!array_key_exists($c->sqlColumn(), $this->columns)) throw new exception('Column '.$c->column.' does not exist in table '.$this->schema.'.'.$this->name);
 		}
 
 		$this->filters[] = $f;
@@ -85,97 +85,7 @@ class entity {
 		$this->filters = array();
 	}
 
-/**
- * @brief Create a new record in the database
- * The record will have its column values set using any setValue operations that have been executed since the entity was created or since clearValues was last issued.
- * Note that any columns not populated in this way must either allow null values or have a default defined in the table definition.
- */
-	function create($forceful = false) {
-		$cols = '';
-		$vals = '';
-		$into = $this->schema.".".$this->name;
-
-		foreach ($this->columns as $col => $def) {
-			if (isset($def['value'])) {
-				$cols .= ($cols == '' ? '' : ', ').$def['column_name'];
-				$vals .= ($vals == '' ? '' : ', ')."'".$this->store->real_escape_string($def['value'])."'";
-			}
-		}
-
-		$sql = "INSERT INTO $into ($cols) VALUES ($vals)";
-
-		if (!$this->store->query($sql)) throw new exception($this->store->error);
-	}
-
-/**
- * @brief Modify a database record
- * All records matching any filters put on the entity will be modified.
- * If no filters exist, the modification will apply to all records in the base table. This will cause an exception to be raised unless the $forceful parameter is set true.
- * The columns to be changed and their new values should be set using one setValue operation for each column.
- * @param $forceful Allow all records to be modified by a single call
- */
-	function modify($forceful = false) {
-		$sets = '';
-		$from = $this->schema.".".$this->name;
-		$where = '';
-
-		foreach ($this->columns as $col => $def) {
-			if (isset($def['value'])) $sets .= ($sets == '' ? '' : ', ').$def['column_name']."='".$this->store->real_escape_string($def['value'])."'";
-			if (isset($def['operand'])) $where .= ($where == '' ? '' : ' AND ').$def['column_name']." ".$def['operator']." '".$this->store->real_escape_string($def['operand'])."'";
-		}
-
-		if ($sets == '') throw new exception ("No columns to update");
-		if (!$forceful && ($where == '')) throw new exception ("Attempt to update all rows ignored");
-
-		$sql = "UPDATE $from SET $sets".($where == '' ? "" : " WHERE $where");
-
-		if (!$this->store->query($sql)) throw new exception($this->store->error);
-	}
-
-/**
- * @brief Remove records from the database table
- * This method will remove all records from the entity's base table that match any of the filters that have been applied to it.
- * If no filters exist, the modification will apply to all records in the base table. This will cause an exception to be raised unless the $forceful parameter is set true.
- * @param $forceful Allow all records to be removed by a single call
- */
-	function remove($forceful = false) {
-		$from = $this->schema.".".$this->name;
-		$where = '';
-
-		foreach ($this->columns as $col => $def) {
-			if (isset($def['operand'])) $where .= ($where == '' ? '' : ' AND ').$def['column_name']." ".$def['operator']." '".$this->store->real_escape_string($def['operand'])."'";
-		}
-
-		if (!$forceful && ($where == '')) throw new exception ("Attempt to delete all rows ignored");
-
-		$sql = "DELETE FROM $from".($where == '' ? "" : " WHERE $where");
-
-		if (!$this->store->query($sql)) throw new exception($this->store->error);
-	}
-
-/**
- * @brief Gets all applicable entity data
- * This method returns an array containing all data in the base table that match any of the filters currently applied to the entity.
- * The return value is an array of arrays of the following structure:
- *
- * `Array`
- * `(`
- * `   [<rownum>] => Array`
- * `      (`
- * `          [<table name>] => Array`
- * `              (`
- * `                  [id] => <value>`
- * `                  [name] => <column name>`
- * `              )`
- * `      )`
- * `)`
- *
- * @param $showKeys If set true, the return array will contain all the primary and foreign key values in the base table (these will be hidden otherwise)
- * @returns An array of data from the base table that match the currently defined entity
- */
-	function data($showKeys = false) {
-		$ret = array();
-
+	private function selectFromWhere($showKeys, &$select, &$from, &$where) {
 		$select = '';
 		$from = '';
 		$where1 = '';
@@ -203,7 +113,7 @@ class entity {
 				$subclause = '';
 
 				foreach ($f->constraints as $c) {
-					$subclause .= ($subclause == '' ? '' : ' AND ')."$t1.".$c->column." ".$c->operator." '".$this->store->real_escape_string($c->operand)."'";
+					$subclause .= ($subclause == '' ? '' : ' AND ')."$t1.".$c->sqlColumn()." ".$c->sqlOperator()." ".$c->sqlOperand($table->store)."";
 				}
 
 				if ($subclause !== '') $where1 .= ($where1 == '' ? '' : ' OR ')."($subclause)";
@@ -240,10 +150,98 @@ class entity {
 		$where = '';
 		$where .= ($where1 == '' ? "" : ($where == '' ? " WHERE " : " AND ")."($where1)");
 		$where .= ($where2 == '' ? "" : ($where == '' ? " WHERE " : " AND ")."($where2)");
+	}
+
+/**
+ * @brief Create a new record in the database
+ * The record will have its column values set using any setValue operations that have been executed since the entity was created or since clearValues was last issued.
+ * Note that any columns not populated in this way must either allow null values or have a default defined in the table definition.
+ */
+	function create($forceful = false) {
+		$cols = '';
+		$vals = '';
+		$into = $this->schema.".".$this->name;
+
+		foreach ($this->columns as $col => $def) {
+			if (isset($def['value'])) {
+				$cols .= ($cols == '' ? '' : ', ').$def['column_name'];
+				$vals .= ($vals == '' ? '' : ', ')."'".$this->store->real_escape_string($def['value'])."'";
+			}
+		}
+
+		$sql = "INSERT INTO $into ($cols) VALUES ($vals)";
+
+		if (!$this->store->query($sql)) throw new exception($this->store->error);
+	}
+
+/**
+ * @brief Modify a database record
+ * All records matching any filters put on the entity will be modified.
+ * If no filters exist, the modification will apply to all records in the base table. This will cause an exception to be raised unless the $forceful parameter is set true.
+ * The columns to be changed and their new values should be set using one setValue operation for each column.
+ * @param $forceful Allow all records to be modified by a single call
+ */
+	function modify($forceful = false) {
+		$sets = '';
+		$this->selectFromWhere(false, $select, $from, $where);
+		//$from = $this->schema.".".$this->name;
+
+		foreach ($this->columns as $col => $def) {
+			if (isset($def['value'])) $sets .= ($sets == '' ? '' : ', ').$def['column_name']."='".$this->store->real_escape_string($def['value'])."'";
+		}
+
+		if ($sets == '') throw new exception ("No columns to update");
+		if (!$forceful && ($where == '')) throw new exception ("Attempt to update all rows ignored");
+
+		$sql = "UPDATE $from SET $sets".($where == '' ? "" : " WHERE $where");
+
+		if (!$this->store->query($sql)) throw new exception($this->store->error);
+	}
+
+/**
+ * @brief Remove records from the database table
+ * This method will remove all records from the entity's base table that match any of the filters that have been applied to it.
+ * If no filters exist, the modification will apply to all records in the base table. This will cause an exception to be raised unless the $forceful parameter is set true.
+ * @param $forceful Allow all records to be removed by a single call
+ */
+	function remove($forceful = false) {
+		$this->selectFromWhere(false, $select, $from, $where);
+
+		if (!$forceful && ($where == '')) throw new exception ("Attempt to delete all rows ignored");
+
+		$sql = "DELETE FROM $from".($where == '' ? "" : " WHERE $where");
+
+		if (!$this->store->query($sql)) throw new exception($this->store->error);
+	}
+
+/**
+ * @brief Gets all applicable entity data
+ * This method returns an array containing all data in the base table that match any of the filters currently applied to the entity.
+ * The return value is an array of arrays of the following structure:
+ *
+ * `Array`
+ * `(`
+ * `   [<rownum>] => Array`
+ * `      (`
+ * `          [<table name>] => Array`
+ * `              (`
+ * `                  [id] => <value>`
+ * `                  [name] => <column name>`
+ * `              )`
+ * `      )`
+ * `)`
+ *
+ * @param $showKeys If set true, the return array will contain all the primary and foreign key values in the base table (these will be hidden otherwise)
+ * @returns An array of data from the base table that match the currently defined entity
+ */
+	function data($showKeys = false) {
+		$this->selectFromWhere($showKeys, $select, $from, $where);
 
 		$sql = "SELECT $select FROM $from$where";
 
 		if (!$result = $this->store->query($sql)) throw new exception($this->store->error);
+
+		$ret = array();
 
 		while ($row = $result->fetch_assoc()) {
 			$data = array();
@@ -305,10 +303,11 @@ class constraint {
 	const GE = 6;	/**< @brief Greater or equal	*/
 	const GTE = 7;	/**< @brief Greater or equal	*/
 	const NE = 8;	/**< @brief Not equal		*/
+	const IN = 9;	/**< @brief IN			*/
 
-	public $column;
-	public $operand;
-	public $operator;
+	private $column;
+	private $operand;
+	private $operator;
 
 /**
  * @param $column Name of the table column
@@ -317,11 +316,24 @@ class constraint {
  */
 
 	function __construct($column, $operand, $operator = constraint::EQ) {
-		$ops=array(constraint::EQ => '=', constraint::LT => '<', constraint::GT => '>', constraint::LE => '<=', constraint::LTE => '<=', constraint::GE => '>=', constraint::GTE => '>=', constraint::NE => '<>');
-
 		$this->column = $column;
 		$this->operand = $operand;
-		$this->operator = $ops[$operator];
+		$this->operator = $operator;
+	}
+
+	function sqlColumn() {
+		return $this->column;
+	}
+
+	function sqlOperand($store) {
+		if ($this->operator == constraint::IN) return '('.$this->operand.')';
+		else return "'".$store->real_escape_string($this->operand)."'";
+	}
+
+	function sqlOperator() {
+		$ops = array(constraint::EQ => '=', constraint::LT => '<', constraint::GT => '>', constraint::LE => '<=', constraint::LTE => '<=', constraint::GE => '>=', constraint::GTE => '>=', constraint::NE => '<>', constraint::IN => 'IN');
+
+		return $ops[$this->operator];
 	}
 }
 
