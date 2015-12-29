b6b4fa71e60dd17dcad24b14da5e1748b3b39e48
Dave Hindle <dave@isware.co.uk>
2015-12-29 15:14:23 +0000

    Updates

--- html/search/functions_1.js
+++ html/search/functions_1.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['add',['add',['../classfilter.html#ab8a6f36222725436d225a0494867147c',1,'filter']]],
-  ['addfilter',['addFilter',['../classentity.html#a7041812e724f4d4e92f7350ac1ca4730',1,'entity']]]
+  ['addfilter',['addFilter',['../classentity.html#a7041812e724f4d4e92f7350ac1ca4730',1,'entity']]],
+  ['apply',['apply',['../classentity.html#ad0ef3e5b12dcd2964ac88226e64851ed',1,'entity']]]
 ];
